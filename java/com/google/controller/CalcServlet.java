8a657b152c653db84c048c086aa19acdc47f7489
Harsh Garg <harshgarg1632000@gmail.com>
2023-04-01 16:50:49 +0530

    validation

--- java/com/google/controller/CalcServlet.java
+++ java/com/google/controller/CalcServlet.java
@@ -1,25 +1,30 @@
 package com.google.controller;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class CalcServlet extends HttpServlet {
-	public void service(HttpServletRequest request, HttpServletResponse response) {
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		int number_1 = Integer.parseInt(request.getParameter("number_1"));
 		int number_2 = Integer.parseInt(request.getParameter("number_2"));
 		String operation = request.getParameter("operation");
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
 		if(operation.equals("add")) {
-			System.out.println("Addition: "+(number_1+number_2));
+			out.println(number_1+" + " +number_2 +" = "+ (number_1+number_2)+"<br/>");
 		}
 		else if (operation.equals("subtract")) {
-			System.out.println("Subtraction: "+(number_1-number_2));
+			out.println(number_1+" - "+number_2+" = "+(number_1-number_2)+"<br/>");
 		}
 		else if (operation.equals("multiply")) {
-			System.out.println("Multiplication: "+(number_1*number_2));
+			out.println(number_1+" x "+number_2+" = "+(number_1*number_2)+"<br/>");
 		}
 		else if(operation.equals("divide")){
-			System.out.println("Division: "+(number_1/number_2));
+			out.println(number_1+" / "+number_2+" = "+(number_1/number_2)+"<br/>");
 		}
 	}
 }
--- java/com/google/controller/SignupController.java
+++ java/com/google/controller/SignupController.java
@@ -1,20 +1,62 @@
 package com.google.controller;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class SignupController extends HttpServlet  {
 	
-	public void service(HttpServletRequest request, HttpServletResponse response) {
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 //		System.out.println("Signup");
 		
 		String firstName = request.getParameter("firstName");
 		String email = request.getParameter("email");
 		String password = request.getParameter("password");
 		
-		System.out.println("First Name: "+firstName);
-		System.out.println("Email: "+email);
-		System.out.println("Password: "+password);
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		boolean isError = false;
+		StringBuffer error = new StringBuffer("");
+		String fNameCheck = "[A-Za-z]+$";
+		String emailCheck = "[a-z0-9]+@[a-z.]+[a-z]";
+		String passwordCheck = "^.*(?=.{8,})(?=..*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).*$";
+		if(firstName==null || firstName.trim().length()==0) {
+			isError=true;
+			error.append("<br> Please Enter FirstName");
+		}
+		else if (firstName.matches(fNameCheck) == false) {
+			isError=true;
+			error.append("<br> Please Enter valid FirstName");
+		}
+		
+		if(email==null || email.trim().length()==0) {
+			isError=true;
+			error.append("<br> Please Enter Email");
+		}
+		else if (email.matches(emailCheck)==false) {
+			isError=true;
+			error.append("<br> Please Enter valid Email");
+		}
+		
+		if(password==null || password.trim().length()==0) {
+			isError=true;
+			error.append("<br> Please Enter Password");
+		}
+		else if (password.matches(passwordCheck)==false) {
+			isError=true;
+			error.append("<br> Please Enter valid Password");
+		}
+		if (isError == true) {
+			out.print("<p style='color:red;'>"+error+"</p>");
+		}
+		else {
+		out.println("First Name: "+firstName+"<br>");
+		out.println("Email: "+email+"<br>");
+		out.println("Password: "+password+"<br>");
+		}
 	}
 }
