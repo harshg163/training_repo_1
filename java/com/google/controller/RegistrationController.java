d36390a5e691ad608cf2fd2c664a8ec12bd5843e
Harsh Garg <harshgarg1632000@gmail.com>
2023-04-01 17:06:01 +0530

    validation

---
+++ java/com/google/controller/RegistrationController.java
@@ -0,0 +1,104 @@
+package com.google.controller;
+
+import java.io.IOException;
+//import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Registration")
+public class RegistrationController extends HttpServlet {
+
+	@Override
+	 protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	 {
+		String firstName = request.getParameter("firstName");
+		String lastName = request.getParameter("lastName");
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+		String cPassword = request.getParameter("cPassword");
+		String gender = request.getParameter("gender");
+		String city = request.getParameter("city");
+		String hobbies = request.getParameter("hobbies");
+		
+		boolean isError = false;
+		StringBuffer error = new StringBuffer("");
+		String fNameCheck = "[A-Za-z]+$";
+		String lNameCheck = "[A-Za-z]+$";
+		String emailCheck = "[a-z0-9]+@[a-z.]+[a-z]";
+		String passwordCheck = "^.*(?=.{8,})(?=..*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).*$";
+		if(firstName==null || firstName.trim().length()==0) {
+			isError=true;
+			error.append("<br> <span style='color:red;'>Please Enter FirstName</span>");
+		}
+		else if (firstName.matches(fNameCheck) == false) {
+			isError=true;
+			error.append("<br> <span style='color:red;'>Please Enter valid FirstName</span>");
+		}
+		if(lastName==null || lastName.trim().length()==0) {
+			isError=true;
+			error.append("<br> <span style='color:red;'>Please Enter FirstName</span>");
+		}
+		else if (lastName.matches(lNameCheck) == false) {
+			isError=true;
+			error.append("<br> <span style='color:red;'>Please Enter valid FirstName</span>");
+		}
+		
+		if(email==null || email.trim().length()==0) {
+			isError=true;
+			error.append("<br> <span style='color:red;'>Please Enter Email</span>");
+		}
+		else if (email.matches(emailCheck)==false) {
+			isError=true;
+			error.append("<br> <span style='color:red;'>Please Enter valid Email</span>");
+		}
+		
+		if(password==null || password.trim().length()==0) {
+			isError=true;
+			error.append("<br> <span style='color:red;'>Please Enter Password</span>");
+		}
+		else if (password.matches(passwordCheck)==false) {
+			isError=true;
+			error.append("<br> <span style='color:red;'>Please Enter valid Password</span>");
+		}
+		
+		if(cPassword==null || cPassword.trim().length()==0) {
+			isError=true;
+			error.append("<br> <span style='color:red;'>Please confirm your password</span>");
+		}
+		else if (!cPassword.equals(password)) {
+			isError=true;
+			error.append("<br> <span style='color:red;'>Password didn't match, please try again</span>");
+		}
+		
+		if(gender==null || gender.trim().length()==0) {
+			isError=true;
+			error.append("<br><span style='color:red;'>Please select your gender</span>");
+		}
+		if(city==null || city.trim().length()==0) {
+			isError=true;
+			error.append("<br><span style='color:red;'>Please select your city</span>");
+		}
+		if(hobbies==null || hobbies.trim().length()==0) {
+			isError=true;
+			error.append("<br><span style='color:red;'>Please select your hobbies</span>");
+		}
+		
+		RequestDispatcher rd = null;
+		if (isError == true) 
+		{
+			request.setAttribute("error", error.toString());
+			rd = request.getRequestDispatcher("Registration.jsp");
+		} else 
+		{
+
+			rd = request.getRequestDispatcher("Login.jsp");
+		}
+			rd.forward(request, response);
+	 }
+}
+
---
+++ webapp/Registration.jsp
@@ -0,0 +1,32 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1" pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Registration</title>
+</head>
+<body>
+	<% String error = (String)request.getAttribute("error"); %>
+	<form action="Registration" method="post">
+	FirstName* : <input type="text" name="firstName"><br/><br/>
+	LastName* : <input type="text" name="lastName"><br/><br/>
+	Gender* : <input type="radio" name="gender" value="Male">Male
+			<input type="radio" name="gender" value="Female">Female<br/><br/>
+	Email* : <input type="email" name="email"><br/><br/>
+	Password* : <input type="password" name="password"><br/><br/>
+	Confirm Password* : <input type="password" name="cPassword"><br/><br/>
+	City* : <select name="city">
+			<option value="">Select Your City</option>
+			<option value="Jaipur">Jaipur</option>
+			<option value="Ahmedabad">Ahmedabad</option>
+			<option value="Mumbai">Mumbai</option>
+		</select>
+		<br/><br/>
+	Hobbies* : <input type="checkbox" name="hobbies" value="travelling">Travelling
+			 <input type="checkbox" name="hobbies" value="Watching Matches">Watching Movies<br/><br/>	
+	<input type="submit" value="Register">
+	</form>
+	<br/><br/>
+	<%=error %>
+</body>
+</html>
